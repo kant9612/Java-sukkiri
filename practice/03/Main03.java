d8d98a76b323581eeb1c696819ac33595c522914
kant9612 <kanstephentakayama@gmail.com>
2021-05-25 06:01:23 +0000

    練習問題3-3

---
+++ practice/03/Main03.java
@@ -0,0 +1,20 @@
+public class Main03 {
+	public static void main(String[] args) {
+		int seibutsu = 0;
+		int age = 24;
+
+		System.out.println("こんにちは。");
+
+		if(seibutsu == 0){
+		    System.out.println("私は男です。");
+		}else {
+		    System.out.println("私は女です。");
+		}
+
+		if(seibutsu == 0){
+		    System.out.println(age + "歳です。");
+		}
+
+		System.out.println("よろしくおねがいします。");
+	}
+}
