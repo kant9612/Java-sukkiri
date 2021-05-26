c666c7a1cddedd9c589465785adc59b403fc0b82
kant9612 <kanstephentakayama@gmail.com>
2021-05-26 08:51:06 +0000

    拡張for文

---
+++ practice/04/Main11.java
@@ -0,0 +1,8 @@
+public class Main11 {
+	public static void main(String[] args) {
+	    int[] score = {20, 30, 40, 50, 80};
+	    for(int value : score){
+	        System.out.println(value);
+	    }
+	}
+}
