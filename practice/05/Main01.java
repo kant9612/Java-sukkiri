7df8b5c34ceb128d35ad4439b5540060595dd730
kant9612 <kanstephentakayama@gmail.com>
2021-05-27 13:15:45 +0000

    メソッドの呼び出し

---
+++ practice/05/Main01.java
@@ -0,0 +1,12 @@
+public class Main01 {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します。");
+// 		メソッドの呼び出し
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました。");
+	}
+
+	public static void hello(){
+	    System.out.println("奏さん、こんにちは！");
+	}
+}
