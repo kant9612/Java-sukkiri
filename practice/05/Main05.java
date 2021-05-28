6d20cdf45fe6ebe903859582c9be513c49fefe1e
kant9612 <kanstephentakayama@gmail.com>
2021-05-28 14:21:19 +0000

    引数2つの場合

---
+++ practice/05/Main05.java
@@ -0,0 +1,11 @@
+public class Main05 {
+	public static void main(String[] args) {
+		add(100, 20);
+		add(200, 20);
+	}
+
+	public static void add(int x, int y){
+	    int ans = x + y;
+	    System.out.println(x + "+" + y + "=" + ans);
+	}
+}
